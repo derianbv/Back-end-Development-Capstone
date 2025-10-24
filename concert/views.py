2016fc2b2a14f03c2337b04a44f74fb05e4360c3
derianbv <derojasl@unal.edu.co>
2025-10-24 11:37:36 -0400

    Cambios :p

--- concert/views.py
+++ concert/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth.hashers import make_password
 
@@ -14,7 +14,27 @@ import requests as req
 # Create your views here.
 
 def signup(request):
-    pass
+    if request.method == "GET": 
+        return render(request, "signup.html", {"form": SignUpForm})
+        
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.filter(username=username).first()
+            if user:
+                return render(request, "signup.html", {"form": SignUpForm, 'message':"user already registered" }) #{insérer le code pour rendre la page signup.html avec le formulaire SignUpForm et un message "l'utilisateur existe déjà"}
+            else:
+                user = User.objects.create(username=username, password=make_password(password))  #{insérer le code pour créer un nouvel utilisateur en utilisant la méthode User.objects.create. N'oubliez pas d'utiliser la méthode make_password pour créer le mot de passe de manière sécurisée}
+                login(request, user) #{insérer le code pour connecter l'utilisateur avec le module django.contrib.aut.}
+                
+                return redirect('index')  #{insérer le code pour retourner l'utilisateur à la page d'index}
+        except User.DoesNotExist:
+            return render(request, "signup.html", {"form": SignUpForm})
+    return render(request, "signup.html", {"form": SignUpForm})
+
+
+
 
 
 def index(request):
@@ -22,15 +42,23 @@ def index(request):
 
 
 def songs(request):
-    # songs = {"songs":[]}
-    # return render(request, "songs.html", {"songs": [insert list here]})
-    pass
+    songs = {"songs":[{"id":1,"title":"duis faucibus accumsan odio curabitur convallis","lyrics":"Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis."}]}
+    return render(request, "songs.html", {"songs": songs["songs"]})
+    
 
 
 def photos(request):
-    # photos = []
-    # return render(request, "photos.html", {"photos": photos})
-    pass
+    photos = [{
+    "id": 1,
+    "pic_url": "http://dummyimage.com/136x100.png/5fa2dd/ffffff",
+    "event_country": "United States",
+    "event_state": "District of Columbia",
+    "event_city": "Washington",
+    "event_date": "11/16/2022"
+}]
+
+    return render(request, "photos.html", {"photos": photos})
+
 
 def login_view(request):
     pass
